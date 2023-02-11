314a2a1bcd6960dc5630cbe76ac31a4edede3a08
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-02-10 22:06:43 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Tipos de datos en Python
+#Int: a= 1
+#Float b= 1.0
+#String c= "Hola"
+#Boolean d= True|False
+#List e= [1,2,3,4,5] (Mutable)
+#Tupla  f= (1,2,3,4,5) (Inmutable)
+#Dictionario g= {"a":1,"b":2,"c":3}
+
+
