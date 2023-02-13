28f3a2c0724b0013b14bce0c8c048ef41abcb065
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-02-12 19:59:43 -0600

    intro datos

---
+++ CLASE1/clase1.py
@@ -0,0 +1,24 @@
+# Tipos de datos básicos
+# Int: números enteros
+x = 10
+# print(x+5)
+# float: números decimales
+y = 0.5
+# print(0.5+y)
+# string: texto
+z = '3000'
+# print(z)
+
+# cadena_multilinea = """Hola como estan les mando un saludo
+#
+# -- atentamente: yo
+# dsafa
+# fadsf
+# """
+
+# prueba = "Hola \n Adios"
+
+# print(prueba)
+# ------------ concatenacion de cadenas ------------------
+print("hola" + "Juan")
+print("hola mi salario es: " + z)
