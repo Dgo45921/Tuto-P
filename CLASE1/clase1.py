136e96f1390e229c9589f3a1f803f1a03dad882c
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-02-12 20:25:26 -0600

    intro datos

--- CLASE1/clase1.py
+++ CLASE1/clase1.py
@@ -1,12 +1,12 @@
 # Tipos de datos básicos
 # Int: números enteros
-x = 10
+# x = 10
 # print(x+5)
 # float: números decimales
-y = 0.5
+# y = 0.5
 # print(0.5+y)
 # string: texto
-z = '3000'
+# z = '3000'
 # print(z)
 
 # cadena_multilinea = """Hola como estan les mando un saludo
@@ -20,5 +20,18 @@ z = '3000'
 
 # print(prueba)
 # ------------ concatenacion de cadenas ------------------
-print("hola" + "Juan")
-print("hola mi salario es: " + z)
+# print("hola" + "Juan")
+# print("hola mi salario es: " + z)
+
+# BOOLEANOS ------------------------------------------------------------------------
+# hola = True
+# adios = False
+# print(hola * adios)
+
+
+# inputs ----------------------------------------------------------------------
+# print("Dame un número")
+# entrada = input("dame un número: ")
+# entrada2 = input("dame otro número: ")
+# print(int(entrada))  # castear
+# cuando se suma entero y float obtenemos float
